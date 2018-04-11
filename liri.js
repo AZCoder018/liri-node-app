66dd3baf9a41cf7a4f17c3e63f3c9e8eb1ba963f
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-04-11 15:43:23 -0700

    liri.js revisions

--- liri.js
+++ liri.js
@@ -1,11 +1,22 @@
+//import API keys from .env file
 require("dotenv").config();
 
+//import keys file
 var keys = require("./keys.js");
 
-//to include the Twitter npm package
+//require fs library
+var fs = require("fs");
+
+//import Twitter npm package
 var Twitter = require("twitter");
 
-//pulling Twitter API info from keys.js
+//import Node Spotify API npm package
+var Spotify = require('node-spotify-api');
+
+//import Request API npm package for OMDB
+var request = require('request');
+
+//keys for pulling Twitter API6
 var twitter = new Twitter ({
   	consumer_key: keys.twitter.consumer_key,
   	consumer_secret: keys.twitter.consumer_secret,
@@ -13,233 +24,139 @@ var twitter = new Twitter ({
   	access_token_secret: keys.twitter.access_token_secret
 });
 
-//the twitter account that will be pulled up
-//potential for later: change this part so the user is asked to input a twitter screen name
+//Twitter user account
 var myTweets = {screen_name: "MissULucy550"};
 
-
-
-//to include the Node Spotify API npm package
-var Spotify = require('node-spotify-api');
-
-//pulling Node Spotify API info from keys.js
+//keys for pulling Spotify API
 var spotify = new Spotify({
 	id: keys.spotify.id,
 	secret: keys.spotify.secret
 });
 
-//variable to hold song the user wants to search for with Spotify
+//variable for Spotify song search
 var songChoice = "";
 
-
-
-//to include the Request API npm package
-//this will be used for the Open Movie Database (OMDB)
-var request = require('request');
-
-//the key to use OMDB
-//var omdbKey = "40e9cece"; //Just for reference. I ended up not using this variable
-
-//variable to hold movie the user wants to search for with OMDB
+//variable for OMDB movie search
 var omdbRequest = "";
 
-
-
-//to include the fs library
-var fs = require("fs");
-
-
-
-//this variable will be used to store user commands
-/*command options are as follows:
-	my-tweets  			>> shows the last 20 tweets of a particular twitter account
-	spotify-this-song	>> will search for a particular song and display info from Spotify
-	movie-this			>> will search a particular movie and display info from OMDB
-	do-what-it-says		>> will read off of random.txt and perfrom the command written inside the file
-*/
+//variable for user commands
 var userCommand = process.argv[2];
-
-
-
-//======================= THE CALLS =======================
-
 
 //for the twitter calls
 if (userCommand === "my-tweets") {
-
 	twitterCall();
 }
 
-
 //for the spotify calls 
 else if (userCommand === "spotify-this-song") {
-
 	spotifyCheck();
 	spotifyCall(songChoice);
 }
 
-
 //for the open movie database (OMDB) calls
 else if (userCommand === "movie-this") {
-
 	movieCheck();
 	movieCall(omdbRequest);
 }
 
-
-
 //for the do-what-it-says call
 else if (userCommand === "do-what-it-says") {
-
 	justDoIt();	
 }
 
-
-//if the user forgets to put in a command for process.argv[2]
-//or if the command is spelled incorrectly
+//if user does not input a command, return "There was an error."
 else {
 	return console.log("There was an error.");
 }
 
-
-
-
-//======================= THE FUNCTIONS =======================
-
-
 //function for the twitter calls
 function twitterCall() {
-	
 	twitter.get("statuses/user_timeline", myTweets, function(error, tweets, response){
-
 		if(error) {
 			return console.log(error);
-
 		} else {
+		console.log("Tweets from " + myTweets.screen_name + ":");
+		console.log("");
+			for (var i = 0; i < tweets.length; i++) {
 
-			console.log("Tweets from " + myTweets.screen_name + ":");
-			console.log("");
+			//convert UTC time to local time for tweet timestamp 
+			var tweetDate = new Date(tweets[i].created_at);
 
-			for (var h = 0; h < tweets.length; h++) {
+			console.log(tweets[i].text);
 
-				//convert UTC time to local time for tweet timestamp part 1
-				var tweetDate = new Date(tweets[h].created_at);
-
-				console.log(tweets[h].text);
-
-				//convert UTC time to local time for tweet timestamp part 2
-				console.log(tweetDate.toString()); 
-				console.log("------------------------------------------");
-
-				//console.log(JSON.stringify(tweets, null, 2)); //<< prints out entire response of call. saving for reference.
+			//convert UTC time to local time for tweet timestamp 
+			console.log(tweetDate.toString()); 
+			console.log("------------------------------------------");
 			}
-
 		}
-
-
 	});
-
 };
 
-
-
-
-//functions for the spotify calls
-//spotifyCheck() figures out what the user is searching and puts it in variable songChoice
-//spotifyCall() executes the search with songChoice
+//functions for Spotify calls; spotifyCheck function puts user's songChoice into var songChoice
+//spotifyCall function executes the search with songChoice
+//make for-loop for multi-word song titles
 function spotifyCheck () {
-	//the song that will be searched
-
     //if the user doesn't put in a song, the default will be The Sign by Ace of Base
 	if (!process.argv[3]) {
 		songChoice = "Ace of Base", "The Sign";
-
 	} else {
-		//this accomodates song titles with multiple words in it
 		for (i = 3; i < process.argv.length; i++) {
 			songChoice = process.argv[i];
-			// console.log(songChoice); //test
 		}
 	}
 };
 
 function spotifyCall(songChoice) {
-
 	spotify.search({type: "track", query: songChoice, limit: 1}, function(error, response){
 		if (error) {
 			return console.log(error);
 		}
 
-		//printing out song information
-		for (var j = 0; j < response.tracks.items[0].album.artists.length; j++) {
-			console.log("Artist(s): " + response.tracks.items[0].album.artists[j].name);
-			console.log("Song: " + response.tracks.items[0].name);
-			console.log("Song Link: " + response.tracks.items[0].external_urls.spotify);
-			console.log("Album: " + response.tracks.items[0].album.name);
+//song information to command line
+	for (var i = 0; i < response.tracks.items[0].album.artists.length; i++) {
+		console.log("Artist(s): " + response.tracks.items[0].album.artists[i].name);
+		console.log("Song: " + response.tracks.items[0].name);
+		console.log("Song Link: " + response.tracks.items[0].external_urls.spotify);
+		console.log("Album: " + response.tracks.items[0].album.name);
 		}
-		
-		//console.log(JSON.stringify(response, null, 2)); //<<keeping this for reference
-
 	});
-
 };
 
-
-
-
-
-
-
-//function for the open movie database (OMDB) calls
-//movieCheck() figures out what the user is searching and puts it in variable omdbRequest
-//movieCall() executes the search with omdbRequest
+//functions for the open movie database (OMDB) calls; movieCheck function puts user's movie request into var omdbRequest//
+//movieCall function executes the search with omdbRequest
+//make for-loop for multi-word movie titles
 function movieCheck() {
 
-	//if the user doesn't put in a movie title, the default search will be Mr. Nobody
+	//default search for movie title "Mr. Nobody" if user does not put in a movie title
 	if (!process.argv[3]) {
 		omdbRequest = "Mr. Nobody";
-
 	} else {
-		//the user's choice will be stored in variable omdbrequest
-		//this accomodates movie titles with multiple words in it
-		for (var k = 3; k < process.argv.length; k++) {
-			omdbRequest += process.argv[k] + "+";
-			//console.log(omdbRequest); //test
+		for (var i = 3; i < process.argv.length; i++) {
+			omdbRequest += process.argv[i] + "+";
 		}
-
 	}
-
 };
 
 function movieCall(omdbRequest) {
-
 	//variable to hold the omdb url search with api key and omdb request
 	var omdbMovie = "http://www.omdbapi.com/?apikey=40e9cece&t=" + omdbRequest;
 
-	
 	request(omdbMovie, function (error, response, body) {
 		if (error) {
 			return console.log(error);
 		}
 
-		//printint out desired information
-		console.log("Title of the movie: " + JSON.parse(body).Title);
-		console.log("Year the movie came out: " + JSON.parse(body).Year);
-		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-		console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-		console.log("Movie language: " + JSON.parse(body).Language);
-		console.log("Movie plot: " + JSON.parse(body).Plot);
-		console.log("Actors in the movie: " + JSON.parse(body).Actors);
-
-
-		//console.log(body);  //<<keeping for reference
+//movie information to command line
+	console.log("Title of the movie: " + JSON.parse(body).Title);
+	console.log("Year the movie came out: " + JSON.parse(body).Year);
+	console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+	console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+	console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+	console.log("Movie language: " + JSON.parse(body).Language);
+	console.log("Movie plot: " + JSON.parse(body).Plot);
+	console.log("Actors in the movie: " + JSON.parse(body).Actors);
 	});
-
 };
-
-
-
-
 
 //function for the do-what-it-says call
 function justDoIt() {
@@ -248,45 +165,27 @@ function justDoIt() {
 			return console.log(error);
 		} else {
 			
-			//splits whatever is in random.txt into strings.
-			//the split occurs wherever a comma exists.
-			//each split string is put into an index in an array call "theSplit"
-			var theSplit = data.split(",");
+//splits random.txt into strings.
+	var theSplit = data.split(",");
+	if (theSplit[0] === "spotify-this-song") {
+		songChoice = theSplit[1];
 
-			if (theSplit[0] === "spotify-this-song") {
-				songChoice = theSplit[1];
-
-				//this will skip over spotifyCheck() and 
-				//execute spotifyCall() with songChoice = whatever is in index 1 of theSplit
-				spotifyCall(songChoice);
+//skip spotifyCheck function and and execute spotifyCall function with songChoice in index 1 of theSplit
+		spotifyCall(songChoice);
 			} 
+	else if (theSplit[0] === "movie-this") {
+		omdbRequest = theSplit[1];
 
-			else if (theSplit[0] === "movie-this") {
-				omdbRequest = theSplit[1];
-
-				//this will skip over omdbCheck() and 
-				//execute omdbCall() with omdbRequest = whatever is in index 1 of theSplit
-				movieCall(omdbRequest);
+//skip over omdbCheck function ABD execute omdbCall function with omdbRequest in index 1 of theSplit
+		movieCall(omdbRequest);
 			}
-
-			else if (theSplit[0] === "my-tweets") {
+	else if (theSplit[0] === "my-tweets") {
 				myTweets.screen_name = theSplit[1];
-				//console.log(myTweets.screen_name); //test
-
-				//execute twitterCall, pulls tweets from whatever account theSplit[1] is
-				//if testing this call out, DO NOT USE " " OR ' ' AROUND THE SCREEN NAME FOR INDEX 1 
-				//IN random.txt!! 
-				twitterCall();
+		twitterCall();
 			}
-
-			else {
-				console.log("Error: There's a problem with this call.")
+	else {
+		console.log("Error: There's a problem with this call.")
 			}
-
-			//console.log(data); //test
-			//console.log(theSplit); //test
-
 		}
-
 	});
 }
